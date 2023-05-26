b8592cdf8f3c19f9b3f9ab40aa6b2327131b4608
Lirovsk <joao.lira@engenharia.ufjf.br>
2023-05-26 15:25:16 -0300

    Modificacao final para o primeiro teste

--- envioDIY.py
+++ envioDIY.py
@@ -1,2 +1,11 @@
 import socket
 import serial
+import time
+
+#declarando a porta serial e a velocidade de transmissão
+ser = serial.Serial('/dev/ttyS0', 9600)
+while True:
+    msgRe=ser.readline()
+    msgleft=ser.inWainting()
+    print(msgRe,msgleft)
+    time.sleep(1)
