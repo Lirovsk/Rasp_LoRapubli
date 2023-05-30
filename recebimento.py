338c99313c0932c248c4c9998ef0370f95f29b0f
Lirovsk <joao.lira@engenharia.ufjf.br>
2023-05-30 13:00:21 -0300

    this last code is the right code to use because it uses the right comunation protocol

---
+++ recebimento.py
@@ -0,0 +1,26 @@
+import spidev
+
+# Inicializar a comunicação SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)  # (bus, device)
+
+# Configurar parâmetros SPI
+spi.max_speed_hz = 500000  # Definir a velocidade de comunicação SPI (500 kHz)
+
+# Função para receber mensagens do dispositivo LoRa
+def receive_message():
+    # Enviar comando para receber dados
+    spi.xfer2([0x00])
+    
+    # Ler até 32 bytes de dados do dispositivo LoRa
+    data = spi.readbytes(32)
+    
+    # Converter os bytes lidos em uma string
+    message = ''.join([chr(byte) for byte in data])
+    
+    # Retornar a mensagem recebida
+    return message
+
+# Exemplo de uso: receber uma mensagem e exibir na tela
+received_message = receive_message()
+print("Mensagem recebida:", received_message)
